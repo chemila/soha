05f2b90796db7d14b256af5763286e0dbf48c335
chemila <chemila@gmail.com>
2011-04-11 14:29:23 +0800

    update database configuration

--- application/config/database.php
+++ application/config/database.php
@@ -17,10 +17,10 @@ return array
 			 *
 			 * Ports and sockets may be appended to the hostname.
 			 */
-			'hostname'   => 'localhost',
-			'database'   => 'pin',
-			'username'   => 'root',
-			'password'   => '',
+			'hostname'   => $_SERVER['SINASRV_DB_HOST'],
+			'database'   => $_SERVER['SINASRV_DB_NAME'],
+			'username'   => $_SERVER['SINASRV_DB_USER'],
+			'password'   => $_SERVER['SINASRV_DB_PASS'],
 			'persistent' => FALSE,
 		),
 		'table_prefix' => 'pin_',
@@ -38,10 +38,10 @@ return array
 			 * string   password    database password
 			 * boolean  persistent  use persistent connections?
 			 */
-			'hostname'   => 'localhost',
-			'database'   => 'pin',
-			'username'   => 'root',
-			'password'   => '',
+			'hostname'   => $_SERVER['SINASRV_DB_HOST_R'],
+			'database'   => $_SERVER['SINASRV_DB_NAME_R'],
+			'username'   => $_SERVER['SINASRV_DB_USER_R'],
+			'password'   => $_SERVER['SINASRV_DB_PASS_R'],
 			'persistent' => FALSE,
 		),
 		/**
