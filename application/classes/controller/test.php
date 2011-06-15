e999664aabd9369d4a0c19bdfa173fbcdc5a585f
chemila <chemila@gmail.com>
2011-06-15 16:14:41 +0800

    add modules

--- application/classes/controller/test.php
+++ application/classes/controller/test.php
@@ -3,14 +3,14 @@
 class Controller_Test extends Controller {
 	public function action_index()
 	{
-        $weibo = new Model_Weibo;
-
-        var_dump($weibo->limit(10)->find_all()->as_array());
+        $this->view = new View('help/index');
+        $this->request->response = $this->view->render();
 	}
 
     public function action_smarty()
     {
-        $this->view = new View('help/index');
-        $this->request->response = $this->view->render();
+        $this->view = new View_Smarty;
+        $this->view->var = 'test';
+        $this->request->response = $this->view->render('smarty:help/test');
     }
 }// End Welcome
