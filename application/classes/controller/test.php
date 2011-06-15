2d490ce7c71b39bb0d26a8dfc99202e49430e0b4
chemila <chemila@gmail.com>
2011-06-15 16:32:32 +0800

    a commit

--- application/classes/controller/test.php
+++ application/classes/controller/test.php
@@ -13,4 +13,20 @@ class Controller_Test extends Controller {
         $this->view->var = 'test';
         $this->request->response = $this->view->render('smarty:help/test');
     }
+
+    public function action_modules()
+    {
+        Cron::set('queue_inbox', array('* * * * *', array($this, 'test')));
+        Cron::run();
+
+        $queue = Queue::instance('native');
+        $queue->create('test');
+        $queue->send('t','hello world');
+        var_dump($queue->receive('t'));
+    }
+
+    public function test()
+    {
+        echo 'test';
+    }
 }// End Welcome
