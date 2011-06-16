847f8ba5b10a2023da042f40cc78cdaac86ec731
ethan <chemila@gmail.com>
2011-06-16 09:16:47 -0700

    a commit

--- application/views/photo/index.php
+++ application/views/photo/index.php
@@ -19,18 +19,18 @@
     </div>
 </div>
 <div id="fullscreendemo">
-<strong>This site requires Flash Player 8.0 or greater</strong><br />
-Please click <a href="http://www.adobe.com/products/flashplayer/">here</a> to download.<br />
-<p>
-email: <a href="mailto:chemila@gmail.com">chemila@gmail.com</a>
+    <strong>This site requires Flash Player 8.0 or greater</strong><br />
+    Please click <a href="http://www.adobe.com/products/flashplayer/">here</a> to download.<br />
+    <p>
+    email: <a href="mailto:chemila@gmail.com">chemila@gmail.com</a>
 </div>
 <script type="text/javascript">
-//<![CDATA[
-var so = new SWFObject("media/swf/photos.swf", "polaroid", "100%", "94%", "8", "#FFFFFF");
-// specify the url to the xml-file, default is photos.xml
-so.addVariable("xmlURL","photo/xml");
-so.write("fullscreendemo");
-//]]>
+    //<![CDATA[
+    var so = new SWFObject("media/swf/photos.swf", "polaroid", "100%", "95%", "8", "#FFFFFF");
+    // specify the url to the xml-file, default is photos.xml
+    so.addVariable("xmlURL","photo/xml");
+    so.write("fullscreendemo");
+    //]]>
 </script>
 </body>
 </html>
