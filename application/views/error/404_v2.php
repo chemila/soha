effd91a70453c9eadf8c1f0e86a20b415383924f
chemila <chemila@gmail.com>
2011-06-16 11:31:29 +0800

    a commit

--- application/views/error/404_v2.php
+++ application/views/error/404_v2.php
@@ -8,20 +8,27 @@ A:link   {color:red; text-decoration:none; cURSOR: default}
 A:visited{color:red; text-decoration:none; cURSOR: default}
 A:active {color:red; text-decoration:none; cURSOR: default}
 A:hover  {color:white; background-color:red; text-decoration:none; font-weight: bold; cURSOR: e-resize; }
+.content {text-align:center}
 </style>
 <body bgcolor="#FFFFFF" MARGINHEIGHT="0" topmargin="0" vspace="0" marginwidth="0" leftmargin="0" hspace="0" style="margin:0">
-    <table width="100%" border="0" cellspacing="0" cellpadding="0" height="100%">
-        <tr align="center" valign="bottom"> 
-            <td>
-                <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=4,0,2,0" width="740" height="480" align="bottom">
-                <param name=movie value="fuckoff3.swf">
-                <param name=quality value=high><param name="SCALE" value="exactfit"><param name="LOOP" value="false">
-                <embed src="media/img/error/fuckoff3.swf" quality=high pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="740" height="480" scale="exactfit" loop="false" align="bottom">
-                </embed> 
-                </object>
-            </td>
-        </tr>
-    </table>
-    <br>
+<table width="100%" border="0" cellspacing="0" cellpadding="0" height="70%">
+    <tr align="center" valign="bottom"> 
+        <td>
+            <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=4,0,2,0" width="600" height="400" align="top">
+            <param name=movie value="fuckoff3.swf">
+            <param name=quality value=high><param name="SCALE" value="exactfit"><param name="LOOP" value="false">
+            <embed src="media/img/error/fuckoff3.swf" quality=high pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="600" height="400" scale="exactfit" loop="false" align="top">
+            </embed> 
+            </object>
+        </td>
+    </tr>
+</table>
+<div class="content">
+    <a href="/">Go home</a> | 
+    <a href="auth">Log in</a> | 
+    <a href="help">Get help</a> | 
+    <a href="mailto:chemila@gmail.com">Email Me</a> | 
+    <a href="http://weibo.com/chemila">Chemila blog</a>
+</div>
 </body>
 </html>
