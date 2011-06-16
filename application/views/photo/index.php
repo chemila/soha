b2becccbd4f60b09f29633ffbad656d4d1ceea52
ethan <chemila@gmail.com>
2011-06-16 08:40:19 -0700

    a commit

--- application/views/photo/index.php
+++ application/views/photo/index.php
@@ -4,95 +4,29 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <base href="<!--{php}-->echo URL::base(false, true);<!--{/php}-->" />
 <title>photos</title>
-<script type="text/javascript" src="media/js/swfobject.js"></script>
-<style type="text/css">
-/* CSS For Polaroid Gallery */
-html, body {
-    height:100%;
-    width:100%;
-    margin:0;
-    color:#4a4d50;
-    background:#e8eae6;
-}
-a {
-    color:#333333;
-    font-weight:bold;
-    text-decoration:none;
-}
-h1 {
-    font-size:30px;
-    margin:15px 0 5px 0;
-}
-h2 {
-    font-size:16px;
-    border-bottom:1px solid #979a9d;
-    margin:0 0 2px 0;
-}
-p {
-    margin:4px 0 10px 0;
-    font-size:10px;
-}
-#wrapper {
-    margin:0 auto;
-    width:815px;
-}
-#header {
-    border-top:5px solid #4a4d50;
-    border-bottom:1px solid #4a4d50;
-    margin:0 0 15px 0;
-}
-#demo {
-    height:600px;
-    width:600px;
-    margin:0 15px 0 0;
-}
-#flickr {
-    background:url(flickr.gif) no-repeat;
-    padding:0 0 0 110px;
-    margin:5px 0 0 0;
-}
-#fullscreendemo {
-    height:100%;
-    width:100%;
-}
-#left {
-    float:left;
-    width:600px;
-}
-#right {
-    width:200px;
-    float:right;
-}
-ul {
-    margin:0;
-    padding:0;
-    margin:4px 0 10px 0;
-}
-ul ul {
-    padding: 0 0 0 20px;
-    margin:0;
-}
-li {
-    font-style:italic;
-    list-style:none;
-    font-weight:bold;
-}
-li li {
-    font-style:normal;
-    list-style:circle;
-    margin:5px 0 0 0;
-    font-weight:normal;
-}
-</style> 
+<script type="text/javascript" src="media/js/photo.js"></script>
+<link href="media/css/photo.css" rel="stylesheet" type="text/css">
 </head>
-
 <body>
-  <div id="fullscreendemo">
-  <strong>This site requires Flash Player 8.0 or greater</strong><br />
-  Please click <a href="http://www.adobe.com/products/flashplayer/">here</a> to download.<br />
-  <p>
-  email: <a href="mailto:chemila@gmail.com">chemila@gmail.com</a>
-  </div>
+<div id="header">
+    <div id="header-inner">
+        <div id="logo">
+            <a href="/">chemila is best</a>
+        </div>
+        <div id="nav">
+            <ul>
+                <li id="nav-home" class="active"><a href="/">首页</a></li>
+                <li id="nav-profile"><a href="u/chemila">我的主页</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div id="fullscreendemo">
+<strong>This site requires Flash Player 8.0 or greater</strong><br />
+Please click <a href="http://www.adobe.com/products/flashplayer/">here</a> to download.<br />
+<p>
+email: <a href="mailto:chemila@gmail.com">chemila@gmail.com</a>
+</div>
 <script type="text/javascript">
 //<![CDATA[
 var so = new SWFObject("media/swf/photos.swf", "polaroid", "100%", "100%", "8", "#FFFFFF");
