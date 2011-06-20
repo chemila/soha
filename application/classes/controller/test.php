b48982dbdbad922e46bc8a4d3d49659df0d3e48c
chemila <chemila@gmail.com>
2011-06-20 18:39:31 +0800

    a commit

--- application/classes/controller/test.php
+++ application/classes/controller/test.php
@@ -35,6 +35,57 @@ HTML;
         }
     }
 
+    public function action_ext()
+    {
+        require_once 'Image/GraphViz.php';
+
+        $graph = new Image_GraphViz();
+
+        $graph->addNode(
+          'Node1',
+          array(
+            'URL'   => 'http://link1',
+            'label' => 'This is a label',
+            'shape' => 'box'
+          )
+        );
+
+        $graph->addNode(
+          'Node2',
+          array(
+            'URL'      => 'http://link2',
+            'fontsize' => '14'
+          )
+        );
+
+        $graph->addNode(
+          'Node3',
+          array(
+            'URL'      => 'http://link3',
+            'fontsize' => '20'
+          )
+        );
+
+        $graph->addEdge(
+          array(
+            'Node1' => 'Node2'
+          ),
+          array(
+            'label' => 'Edge Label'
+          )
+        );
+
+        $graph->addEdge(
+           array(
+             'Node1' => 'Node2'
+           ),
+           array(
+            'color' => 'red'
+         )
+        );
+        echo $graph->image();
+    }
+
     public function action_chart()
     {
         echo <<<HTML
