5a30f6758dcfc4b2d05c23a885b016d4a76b0c75
chemila <chemila@gmail.com>
2011-06-21 17:47:13 +0800

    a commit

--- modules/oauth/classes/oauth/provider/google.php
+++ modules/oauth/classes/oauth/provider/google.php
@@ -2,12 +2,18 @@
 
 class OAuth_Provider_Google extends Kohana_OAuth_Provider {  
     public $name = 'google';
-
     public $signature = 'HMAC_SHA1';
+
+	public function request_token(OAuth_Consumer $consumer, array $params = NULL)
+    {
+        $params['scope'] = 'http://www.google.com/calendar/feeds http://picasaweb.google.com/data';
+
+        return parent::request_token($consumer, $params);
+    }
 
 	public function url_request_token()
 	{
-		return 'http://api.t.sina.com.cn/oauth/request_token';
+		return 'https://www.google.com/accounts/OAuthGetRequestToken';
 	}
 
 	public function url_authorize()
