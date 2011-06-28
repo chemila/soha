20c859168b1320b9100b44bc1d27fe2b9d8ed287
chemila <chemila@gmail.com>
2011-06-28 18:21:15 +0800

    a commit

--- application/views/shared/nav_1.php
+++ application/views/shared/nav_1.php
@@ -3,7 +3,7 @@
         <li class="link active"><a href="javascript:type='weibo';show(page=1);">Home</a></li>
         <li class="link"><a href="javascript:type='user';show(page=1);">Users</a></li>
         <li class="link"><a id="fancy_calendar" href="https://www.google.com/calendar/embed?title=t.pagodabox.com&amp;height=700&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=pagodabox%40gmail.com&amp;color=%232F6213&amp;ctz=Asia%2FShanghai">Calendar</a></li>
-        <li class="link"><a id="fancy_chart" href="test/social">Relationship</a></li>
+        <!--<li class="link"><a id="fancy_chart" href="test/social">Relationship</a></li>-->
         <li class="link"><a href="javascript:show(++page);" title="Get next 20 photos">Next</a></li>
         <li class="link"><a href="javascript:show(--page);" title="Previous 20 photos">Previous</a></li>
         <li class="link"><a id="login" title="Login" href="auth">Login</a></li>
