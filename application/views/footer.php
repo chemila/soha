24b3a6e8402d0dd3120749f3ab0d4e1f0236708c
chemila <chemila@gmail.com>
2011-07-07 11:26:03 +0800

    a commit

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,7 +1,7 @@
 <!--footer start-->
 <!-- JiaThis Button BEGIN -->
 <script type="text/javascript">var jiathis_config = {data_track_clickback:true};</script>
-<script type="text/javascript" src="http://v2.jiathis.com/code/jiathis_r.js?move=0&amp;btn=r1.gif&amp;uid=1523711" charset="utf-8"></script>
+<script type="text/javascript" src="jiathis.js?move=0&amp;btn=r1.gif&amp;uid=1523712" charset="utf-8"></script>
 <!-- JiaThis Button END -->
 </body>
 </html>
